82e672c566582ff05b8f94dfb5e049e230e06449
Joyanna <helloidevthings@gmail.com>
2020-07-01 18:05:09 -0400

    finishing logo grid!

--- src/components/LogoGrid.js
+++ src/components/LogoGrid.js
@@ -11,13 +11,68 @@ const LogoGrid = () => (
     </div>
     <div className="LogoGrid_grid">
       <LogoGridItem name="oak_ridge" type="government" alt="Oak Ridge" />
-      <LogoGridItem name="oak_ridge" type="government" alt="Oak Ridge" />
-      <LogoGridItem name="oak_ridge" type="government" alt="Oak Ridge" />
+      <LogoGridItem
+        name="nnsa"
+        type="government"
+        alt="National Nuclear Security Administration"
+      />
+      <LogoGridItem
+        name="tennessee_state"
+        type="government"
+        alt="Tennessee State Gov"
+      />
+      <LogoGridItem name="pentagon" type="government" alt="Pentagon" />
+      <LogoGridItem name="y12" type="government" alt="Y12" />
+      <LogoGridItem name="us_army" type="government" alt="US Army" />
+      <LogoGridItem
+        name="department_of_defense"
+        type="government"
+        alt="Department of Defense"
+      />
+      <LogoGridItem
+        name="Department_of_Energy"
+        type="government"
+        alt="Department of Energy"
+      />
+      <LogoGridItem
+        name="department_of_Homeland_security"
+        type="government"
+        alt="Department of Homeland Security"
+      />
+      <LogoGridItem
+        name="department_of_justice"
+        type="government"
+        alt="Department of Justice"
+      />
+      <LogoGridItem
+        name="department_of_the_navy"
+        type="government"
+        alt="Department of the Navy"
+      />
+      <LogoGridItem
+        name="department_of_transportation"
+        type="government"
+        alt="Department of Transportation"
+      />
+      <LogoGridItem
+        name="force_protection_agency"
+        type="government"
+        alt="Force Protection Agency"
+      />
+      <LogoGridItem
+        name="general_intellegence_agency"
+        type="government"
+        alt="General Intellegence Agency"
+      />
+      <LogoGridItem
+        name="national_geospacial_intellegence_agency"
+        type="government"
+        alt="National Geospacial Intellegence Agency"
+      />
     </div>
     <div className="LogoGrid_heading">
-      <h3>
-        Media <span className="fancyAND">and</span> Entertainment
-      </h3>
+      <h3>Media</h3>
+      <span className="fancyAND">and</span> <h3>Entertainment</h3>
     </div>
     <div className="LogoGrid_grid">
       <LogoGridItem name="food_network" type="media" alt="Food Network" />
@@ -36,9 +91,9 @@ const LogoGrid = () => (
       />
     </div>
     <div className="LogoGrid_heading">
-      <h3>
-        Transportation <span className="fancyAND">and</span> Automotive
-      </h3>
+      <h3>Transportation</h3>
+      <span className="fancyAND">and</span>
+      <h3>Automotive</h3>
     </div>
     <div className="LogoGrid_grid">
       <LogoGridItem name="auto_zone" type="transportation" alt="Auto Zone" />
@@ -48,9 +103,9 @@ const LogoGrid = () => (
       <LogoGridItem name="idle_air" type="transportation" alt="Idle Air" />
     </div>
     <div className="LogoGrid_heading">
-      <h3>
-        Consumer <span className="fancyAND">and</span> Retail
-      </h3>
+      <h3>Consumer</h3>
+      <span className="fancyAND">and</span>
+      <h3>Retail</h3>
     </div>
     <div className="LogoGrid_grid">
       <LogoGridItem name="verizon" type="consumer" alt="Verizon" />
@@ -65,9 +120,9 @@ const LogoGrid = () => (
       <LogoGridItem name="keurig" type="consumer" alt="Keurig" />
     </div>
     <div className="LogoGrid_heading">
-      <h3>
-        Banking <span className="fancyAND">and</span> Financial
-      </h3>
+      <h3>Banking</h3>
+      <span className="fancyAND">and</span>
+      <h3>Financial</h3>
     </div>
     <div className="LogoGrid_grid">
       <LogoGridItem
@@ -85,9 +140,7 @@ const LogoGrid = () => (
       <LogoGridItem name="ed_financial" type="banking" alt="Ed Financial" />
     </div>
     <div className="LogoGrid_heading">
-      <h3>
-        Banking <span className="fancyAND">and</span> Financial
-      </h3>
+      <h3>Other</h3>
     </div>
     <div className="LogoGrid_grid">
       <LogoGridItem name="ut" type="other" alt="Unniversity of Tennessee" />
