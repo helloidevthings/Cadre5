0b0d393a7700039cdd4ff79ad3fe514dff7c50cb
Joyanna <helloidevthings@gmail.com>
2020-07-15 16:47:01 -0400

    project resolution

--- src/components/FeatureApp.js
+++ src/components/FeatureApp.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Image from "./Parts/Image";
+import { motion, AnimatePresence } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const FeatureApp = () => (
-  <section className="FeatureApp">
-    <Image
-      src="/images/resolution/resolution.png"
-      base="FeatureApp"
-      alt="things"
-    />
-  </section>
-);
+const FeatureApp = ({ img = "", theme }) => {
+  const [ref, inView] = useInView({ triggeronce: true, threshold: 0.5 });
+
+  return (
+    <section className={`FeatureApp FeatureApp--${theme}`}>
+      <div className={`zoom_move ${inView ? "zoom_1" : ""}`} ref={ref}>
+        <Image src={img.src} base="FeatureApp" alt={img.alt} />
+        <div className="ToolTip_wrapper">
+          <AnimatePresence>
+            <motion.div
+              className="ToolTip_dropdown"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <button className="ToolTip" style={{ top: `pink` }}>
+                1
+              </button>
+              <p>description of what this is</p>
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default FeatureApp;
--- src/components/Parts/LogoGridItem.js
+++ src/components/Parts/LogoGridItem.js
@@ -1,7 +1,9 @@
 import React from "react";
+// import { useInView } from "react-intersection-observer";
 
-const LogoGridItem = ({ type, name, alt }) => (
-  <img src={`/images/logos/${type}/${name}.png`} alt={`${alt} Logo`} />
-);
+const LogoGridItem = ({ type, name, alt }) => {
+  // const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.5 });
+  return <img src={`/images/logos/${type}/${name}.png`} alt={`${alt} Logo`} />;
+};
 
 export default LogoGridItem;
