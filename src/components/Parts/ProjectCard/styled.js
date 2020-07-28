ee6631913d2c84ddf802bb13b1d0fd91a8fb9d9d
Joyanna <helloidevthings@gmail.com>
2020-07-28 17:40:44 -0400

    card animations

--- src/components/Parts/ProjectCard/styled.js
+++ src/components/Parts/ProjectCard/styled.js
@@ -1,58 +1,75 @@
 import styled from "@emotion/styled";
 
-export default styled.a`
-  text-decoration: none;
-
-  .ProjectCard {
-    color: ${(props) => props.theme.color.white};
+export default styled.section`
+  & a {
+    color: white;
+    text-decoration: none;
     background-color: ${(props) => props.theme.color.dark};
     flex: 1 1 auto;
     margin: 0.5em;
     max-width: 20em;
     border-radius: 0.7em;
     box-shadow: ${(props) => props.theme.boxShadow.main};
-    transition: all 0.3s ease-in;
+    opacity: 0;
+    transform: scale(1.1);
+    transition: all 0.5s ease-in-out;
+    transition-delay: 0.4s;
+
+    &.active {
+      opacity: 1;
+      transform: none;
+    }
 
     &:hover {
       transform: scale(1.02);
       background-color: ${(props) => props.theme.color.black};
     }
 
-    &_link {
-      text-decoration: none;
+    &:nth-child(2) {
+      transition-delay: 0.6s;
     }
 
-    &_text {
-      padding: 1.3em;
+    &:nth-child(3) {
+      transition-delay: 0.8s;
     }
+  }
 
-    &_media {
-      position: relative;
-      max-height: 280px;
-      overflow: hidden;
+  & .cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 
-      img {
-        width: 100%;
-        height: 100%;
+  & .text {
+    padding: 1.3em;
+  }
+
+  &figure {
+    position: relative;
+    max-height: 280px;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  & .tags {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 1.3em 0;
+
+    li {
+      list-style: none;
+      margin-right: 0.4em;
+      border-radius: 1em;
+      padding: 0.1em 0.8em;
+      &:nth-child(even) {
+        background-color: ${(props) => props.theme.color.grayMd};
       }
-    }
-
-    &_tags {
-      display: flex;
-      flex-wrap: wrap;
-      margin: 1.3em 0;
-
-      li {
-        list-style: none;
-        margin-right: 0.4em;
-        border-radius: 1em;
-        padding: 0.1em 0.8em;
-        &:nth-child(even) {
-          background-color: ${(props) => props.theme.color.grayMd};
-        }
-        &:nth-child(odd) {
-          background-color: ${(props) => props.theme.color.primary};
-        }
+      &:nth-child(odd) {
+        background-color: ${(props) => props.theme.color.primary};
       }
     }
   }
