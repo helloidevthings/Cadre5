b9197a7ffb647c3ed525d3525d6b635cae9200d0
Joyanna <helloidevthings@gmail.com>
2020-09-17 11:31:43 -0400

    removing all CSS files except a sanitize

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,32 +1,35 @@
 import styled from "@emotion/styled";
 
 export default styled.main`
-  /* * {
-    box-sizing: border-box;
-    margin: 0;
-  }
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,500;1,700&display=swap");
 
-  body {
-    font-family: Montserrat, sans-serif;
-    color: black;
-    line-height: 1.59;
-    background: $white;
-    font-weight: 500;
-    text-rendering: optimizeLegibility;
-    font-size: calc(14px + (20 - 14) * ((100vw - 320px) / (2100 - 320)));
-  }
+  /* wrapper around everything */
+  display: flex;
+  flex-wrap: wrap;
+  min-height: 100vh;
+  justify-content: space-between;
 
-  .Main {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-  }
-
+  /* wrapper around main content */
   .Content {
     flex-grow: 9999;
     width: 90%;
+    /* flex-grow: 9999;
+    @supports (max-width: max-content) {
+      min-width: 100px;
+      max-width: max-content;
+      flex-basis: 200px;
+      flex-grow: 99999;
+    } */
   }
 
+  .blog {
+    max-width: 40em;
+    margin: 3em auto;
+    p {
+      margin-top: 1.3em;
+      margin-bottom: 1.3em;
+    }
+  }
   .text-wrapper {
     padding-left: 4em;
     padding-right: 3em;
@@ -38,6 +41,9 @@ export default styled.main`
     text-align: center;
   }
 
+  a {
+    color: ${(props) => props.theme.color.primary};
+  }
   h1,
   h2,
   h3 {
@@ -87,22 +93,21 @@ export default styled.main`
   }
 
   .fancyAND {
-    $main-color: ${(props) => props.theme.color.primary};
-    color: $main-color;
+    color: ${(props) => props.theme.color.primary};
     display: block;
     text-transform: uppercase;
     font-size: 0.9em;
     font-weight: 800;
     position: relative;
     padding-top: 0;
-    padding-bottom: 2px;
-    margin: 9px 0.6em 0;
+    padding-bottom: 0.1rem;
+    margin: 0.6rem 0.6rem 0;
 
     &:before {
       content: "";
       display: block;
       position: absolute;
-      background-color: $main-color;
+      background-color: ${(props) => props.theme.color.primary};
       height: 3px;
       width: 100%;
     }
@@ -113,13 +118,13 @@ export default styled.main`
   }
 
   blockquote {
-    $quote-mark: 1.2em;
+    $quoteMark: 1.2em;
     display: block;
     max-width: 20em;
     margin: 3em auto;
-    padding-left: ($quote-mark * 4);
+    padding-left: 4.4em;
     position: relative;
-    color: ${(props) => props.theme.color.greyMed};
+    color: ${(props) => props.theme.color.grayMed};
     font-size: 1em;
     font-weight: 500;
     font-style: italic;
@@ -127,15 +132,93 @@ export default styled.main`
 
     &:before {
       content: "";
-      color: ${(props) => props.theme.color.greyLt};
+      color: ${(props) => props.theme.color.grayLt};
       font-size: 3em;
       position: absolute;
-      background: url(../images/svgs/quote.svg);
+      background: url(/images/svgs/quote.svg);
       background-repeat: no-repeat;
-      width: $quote-mark;
-      height: $quote-mark;
+      width: 1.2em;
+      height: 1.2em;
       left: 0;
       top: 0.1em;
     }
-  } */
+  }
+
+  /* In-View Triggered Animations */
+  .fadeUp {
+    opacity: 0;
+    transform: translateY(20px);
+    transition: all 0.6s cubic-bezier(0.42, 0, 0.45, 0.99);
+
+    &.active {
+      transform: none;
+      opacity: 1;
+    }
+  }
+
+  .fade_in {
+    opacity: 0.5;
+    transform: translateY(1.8em);
+    transition: all 0.5s ease-in-out;
+  }
+  .fade_1 {
+    opacity: 1;
+    transform: translateY(0em);
+  }
+
+  .zoom_move {
+    transform: scale(0.95) translateY(2em);
+    transition: all 0.5s ease-in;
+  }
+
+  .zoom_1 {
+    transform: scale(1) translateY(0em);
+  }
+
+  /* SVG Animations */
+
+  .draw-in {
+    stroke-dasharray: 1000;
+    stroke-dashoffset: 1000;
+    animation: draw 5s ease-in-out infinite alternate;
+  }
+
+  .drawIN1x {
+    stroke-dasharray: 1000;
+    stroke-dashoffset: 1000;
+    animation: draw 5s ease-in-out;
+  }
+
+  .item-to {
+    animation-duration: 10s;
+    animation-iteration-count: infinite;
+    transform-origin: bottom;
+  }
+
+  .bounce-1 {
+    animation-name: bounce-1;
+    animation-timing-function: ease;
+  }
+
+  @keyframes draw {
+    from {
+      stroke-dashoffset: 1000;
+    }
+
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+
+  @keyframes bounce-1 {
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(50px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
 `;
