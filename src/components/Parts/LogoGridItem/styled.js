007ba596d4824e75f421c0aac39d2358e091e849
Joyanna <helloidevthings@gmail.com>
2020-10-07 15:43:27 -0400

    logo fixes

--- src/components/Parts/LogoGridItem/styled.js
+++ src/components/Parts/LogoGridItem/styled.js
@@ -1,16 +1,15 @@
 import styled from "@emotion/styled";
 
 export default styled.img`
-  max-width: 7em;
-  max-height: 3.75em;
+  max-width: 150px;
+  max-height: 50px;
   margin: 1.3em;
   filter: grayscale(100%);
   opacity: 0.8;
-  /* transition: all 0.1ms ease-in-out; */
+  transition: filter 0.1ms ease-in-out, opacity 0.1ms ease-in-out;
 
   &:hover {
     filter: grayscale(0%) saturate(100%);
-    /* transform: scale(1.3); */
     opacity: 1;
   }
 `;
