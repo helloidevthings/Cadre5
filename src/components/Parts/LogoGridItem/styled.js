4fdb22354317fa2a934418f29e26ab4c3ff11eae
Joyanna <helloidevthings@gmail.com>
2020-10-08 15:12:44 -0400

    adding new logos and links

--- src/components/Parts/LogoGridItem/styled.js
+++ src/components/Parts/LogoGridItem/styled.js
@@ -1,15 +1,17 @@
 import styled from "@emotion/styled";
 
 export default styled.img`
-  max-width: 150px;
-  max-height: 50px;
-  margin: 1.3em;
+  max-width: 8em;
+  max-height: 4em;
+  margin: 0.9em;
   filter: grayscale(100%);
   opacity: 0.8;
-  transition: filter 0.1ms ease-in-out, opacity 0.1ms ease-in-out;
+  transition: transform 0.2s cubic-bezier(0.68, 0.01, 0.68, 1),
+    filter 0.1ms ease-in-out;
 
   &:hover {
     filter: grayscale(0%) saturate(100%);
     opacity: 1;
+    transform: scale(1.1);
   }
 `;
