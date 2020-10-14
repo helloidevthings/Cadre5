7c1f00c583f5f8a40cc688c21c1ff85c6aa42066
Joyanna <helloidevthings@gmail.com>
2020-10-14 14:00:52 -0400

    404 page creation

--- src/components/Pages/FourOhFour.js
+++ src/components/Pages/FourOhFour.js
@@ -1,5 +1,30 @@
 import React from "react";
+import BlogText from "../Parts/BlogText";
+import Button from "../Parts/Button";
+import WaveHR from "../Parts/WaveHR";
+import Image from "../Parts/Image";
+import Page from "../Page";
 
-const FourOhFour = () => <h1 className="FourOhFour">Not Found!</h1>;
+const FourOhFour = () => (
+  <Page>
+    <BlogText textAlign="center">
+      <h1 className="red">Oops!</h1>
+      <h2>404 • Page Not Found!</h2>
+      <p>
+        THe page you are looking for doesn't exist or an other error occured.
+      </p>
+      <Button
+        href="/home"
+        text="Return Home"
+        themeColor="darkTheme"
+        btnStyle="secondary"
+        arrow={true}
+      />
+    </BlogText>
+    {/* <HeroBasic title="Oops! 404" /> */}
+    <Image src="./images/404page.png" />
+    <WaveHR bg="#ffffff" color="#ffffff" />
+  </Page>
+);
 
 export default FourOhFour;
