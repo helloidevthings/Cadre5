eb2c114936399b6cb74e46fa3c6b83f6f7fcce73
Joyanna <helloidevthings@gmail.com>
2020-11-17 22:04:22 -0500

    changes to clients

--- src/components/Pages/Clients.js
+++ src/components/Pages/Clients.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Page from "../Page";
 import HeroBasic from "../HeroBasic";
-import Clients from "../Clients";
 import ProjectsRelated from "../ProjectsRelated";
+import WaveHR from "../Parts/WaveHR";
 import LogoGrid from "../LogoGrid";
 
 export default () => {
@@ -11,14 +11,19 @@ export default () => {
       <HeroBasic
         title="Clients"
         img={{
-          src: "/images/pretty-laptop.png",
+          src: "/images/people_working.png",
           alt: "People working in an office",
         }}
         svg="AbstractWhite"
       />
+            <article className="blog center">
+              <h3>We deliver value to our customers.</h3>
+              <p> Whether that is a new venture like Idle Air, or a National Defense project with Oak Ridge National Lab. We create custom software solutions. We are able to leverage solutions from our experience with a variety of clients.</p>
+           <hr></hr>
+            </article>
+            {/* <WaveHR />  */}
       <LogoGrid />
       <ProjectsRelated />
-
     </Page>
   );
 };
