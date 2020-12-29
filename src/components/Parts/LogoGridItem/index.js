b18b8e119dbd9647d3fd80803a48bddba1c0ee54
Joyanna <helloidevthings@gmail.com>
2020-12-29 14:27:14 -0500

    removing images

--- src/components/Parts/LogoGridItem/index.js
+++ src/components/Parts/LogoGridItem/index.js
@@ -1,20 +1,22 @@
 import React, { Fragment } from "react";
 import Styles from "./styled";
+import {Image} from 'cloudinary-react';
+
 
 export default ({ logos = [] }) => {
   return (
     <Fragment>
       {logos.map(({ type, name, alt, link, delay = false }, i) => (
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          <Styles
-            src={`/images/logos/${type}/${name}.png`}
-            alt={`${alt} Logo`}
-            key={i}
-            className="logo"
-            style={{
-              transitionDelay: `${delay === true ? 0.1 * i : "0"}s `,
-            }}
-          />
+        <a href={link} target="_blank">
+          <Styles>
+              <Image 
+                cloudName="cadre5images" 
+                publicId={`/images/logos/${type}/${name}.png`}
+                alt={`${alt} Logo`}
+                key={i}
+                className="logo"
+                style={{transitionDelay: `${delay === true ? 0.1 * i : "0"}s `}} />
+          </Styles>
         </a>
       ))}
     </Fragment>
