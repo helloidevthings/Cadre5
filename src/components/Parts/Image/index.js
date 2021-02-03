25bb6b5b4abb9ace411a5d55dadfda40cdb429c7
Joyanna <helloidevthings@gmail.com>
2021-02-02 19:27:51 -0500

    changes to G2

--- src/components/Parts/Image/index.js
+++ src/components/Parts/Image/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import { Image } from "cloudinary-react";
 import Styles from "./styled";
 
-export default ({ src, alt, classes }) => {
+export default ({
+  src,
+  alt,
+  classes,
+  figCap,
+}) => {
   return (
     <Styles className={`media ${classes}`}>
       <Image
         cloudName="cadre5images"
         publicId={src}
       ></Image>
+      {figCap && (
+        <figcaption>{figCap}</figcaption>
+      )}
     </Styles>
   );
 };
