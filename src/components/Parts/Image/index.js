a3207f15e3976258869082a348814c54639f5d8a
Joyanna <helloidevthings@gmail.com>
2021-03-18 11:03:00 -0400

    changes to images

--- src/components/Parts/Image/index.js
+++ src/components/Parts/Image/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import Styles from "./styled";
 import Launch from "../../Svgs/Launch";
 
-export default ({ src, alt, classes, figCap }) => {
+export default ({ src, alt, classes, figCap, large }) => {
   return (
     <Styles className={`media ${classes}`}>
       <img
         alt={alt}
         srcset={`images/small${src} 600w, images/large${src} 1600w,
         ${src}`}
-        sizes="(max-width: 600px) 600px,
-        (min-width: 1600px) 1600px"
+        sizes={`(max-width: 600px) 600px, ${large && "(min-width: 1600px) 1600px"}`}
         src={src}
       />
 
