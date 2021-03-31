fb0ad30452b0f10afb717912202e49a1a961e096
Joyanna <helloidevthings@gmail.com>
2021-03-31 12:27:53 -0400

    changes to G2

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,44 +10,26 @@ export default ({ details = [], themeColor }) => {
 
   return (
     <React.Fragment>
-      {details.map(
-        (
-          { subheader, title, leadin, descLg, desc, img = "", link = "" },
-          i
-        ) => (
-          <Styles
-            key={i}
-            className={`${themeColor ? themeColor : ""} ${
-              inView ? "active" : ""
-            }`}
-          >
-            <div className="text-wrapper" ref={ref}>
-              <h2>{title}</h2>
-              <IntroText
-                subheader={subheader}
-                leadin={leadin}
-                descLg={descLg}
-                desc={desc}
+      {details.map(({ subheader, title, leadin, descLg, desc, img = "", link = "" }, i) => (
+        <Styles key={i} className={`${themeColor ? themeColor : ""} ${inView ? "active" : ""}`}>
+          <div className="text-wrapper" ref={ref}>
+            <h2>{title}</h2>
+            <IntroText subheader={subheader} leadin={leadin} descLg={descLg} desc={desc} />
+            {link && (
+              <Button
+                themeColor={link.themeColor}
+                btnStyle={link.btnStyle}
+                href={link.href}
+                text={link.text}
+                arrow={link.arrow}
               />
-              {link && (
-                <Button
-                  themeColor={link.themeColor}
-                  btnStyle={link.btnStyle}
-                  href={link.href}
-                  text={link.text}
-                  arrow={link.arrow}
-                />
-              )}
-            </div>
-            <div
-              className={`image-wrapper ${inView ? "active" : ""}`}
-              ref={ref}
-            >
-              <Image src={img.src} alt={img.alt} />
-            </div>
-          </Styles>
-        )
-      )}
+            )}
+          </div>
+          <div className={`image-wrapper ${inView ? "active" : ""}`} ref={ref}>
+            <Image src={img.src} alt={img.alt} />
+          </div>
+        </Styles>
+      ))}
     </React.Fragment>
   );
 };
