1afe5fb446ccdbbce9682f30ac95e18fae3192aa
Joyanna <helloidevthings@gmail.com>
2021-04-06 21:25:39 -0400

    testing netlify transforms

--- src/components/Parts/Image/index.js
+++ src/components/Parts/Image/index.js
@@ -7,9 +7,9 @@ export default ({ src, alt, classes, figCap }) => {
     <Styles className={`media ${classes}`}>
       <img
         alt={alt}
-        // srcset={`images/_medium${src} 800w`}
-        // sizes="(min-width: 600px) 800px, 600px"
-        src={src}
+        // srcset={`images/${src}?nf_resize=fit&w=300 300w`}
+        // sizes="(min-width: 300px) 300px"
+        src={`${src}?nf_resize=fit&w=300`}
       />
 
       {figCap && (
